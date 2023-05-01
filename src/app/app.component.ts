95ba93aed1d69106cd30a7bf5ccf27659950386b
AnthonyVillavicencio23 <megamrd102@gmail.com>
2023-04-30 19:43:09 -0500

    Prueba de json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent
+{
   title = 'demoCrud';
 }
+
+const http = require('http');
+
+const req = http.get('http://localhost:3000/')
